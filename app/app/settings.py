4cf609e4ce9812b6df88cac2f8d9dbb8e6dab0d7
acmllaugh <acmllaugh@gmail.com>
2019-10-04 23:59:55 +0800

    Change project structure.

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b%)yzf2yv^&g42e^cznydaqk%a#80lnra(5fkjpm3@15vnivx)'
+SECRET_KEY = '1kpfb1mg$#ms8gbsi@%aqf@jm$voq0400w8xc4iz90#u&zhuuj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
